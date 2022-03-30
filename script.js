85f544f61fb1f0b4c45698ec8df2927cce2862db
joey bennett <joeykb17@gmail.com>
2022-03-29 23:06:03 -0700

    add function generatepassword, add prompts, add confirms, add alert, add loops

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var specialCharacters = "!@#$%^&*()_+=,./?";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = lowercase.toLocaleUpperCase;
+  var nums = "0123456789";
+  var pwLength = 0;
+  var hasLowercase, hasUppercase, hasNumbers, hasSpecial = false;
+
+  while (pwLength < 8 || pwLength > 128) {
+    pwLength = prompt("How long should your password be? \n(Between 8-128 characters)");
+  }
+
+  while (!hasLowercase && !hasUppercase && !hasNumbers && !hasSpecial) {
+      hasLowercase = confirm("Do you want lowercase letters in your password?");
+      hasUppercase = confirm("Do you want uppercase letters in your password?");
+      hasNumbers = confirm("Do you want numbers in your password?");
+      hasSpecial = confirm("Do you want special characters in your password?");
+
+    if (!hasLowercase && !hasUppercase && !hasNumbers && !hasSpecial) {
+      alert("You must have at least one type of character set for your password to generate.\nPlease try again.");
+    }
+  }
+  
+  
+}
